e3e20e2b818e05f5f81f0bd508cecf81b6807919
Rishika Randev <rish.ran12@gmail.com>
2024-09-12 23:31:22 -0400

    Fixing test functions

--- test_main.py
+++ test_main.py
@@ -1,20 +1,20 @@
 from main import generate_summary_stats, generate_viz
 
 
-def test_generate_summary_stats(file_name):
+def test_generate_summary_stats():
     """testing out summary stats function"""
-    summary = generate_summary_stats(file_name)
+    summary = generate_summary_stats("StudentPerformanceFactors.csv")
     describe_stats = summary[0]
     medians = summary[1]
     assert describe_stats.loc["mean"]["Previous_Scores"] == 75.07053125472983
     assert medians["Sleep_Hours"] == 7.0
     assert describe_stats.loc["std"]["Physical_Activity"] == 1.0312310926271286
 
-def test_generate_viz(file_name):
+def test_generate_viz():
     """testing out generate viz function"""
-    generate_viz(file_name)
+    generate_viz("StudentPerformanceFactors.csv")
 
 
 if __name__ == "__main__":
-    test_generate_summary_stats("StudentPerformanceFactors.csv")
-    test_generate_viz("StudentPerformanceFactors.csv")
+    test_generate_summary_stats()
+    test_generate_viz()
